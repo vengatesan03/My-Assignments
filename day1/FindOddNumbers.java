5f397394b6a6a2464a038902549c55c9e663fb5d
vengatesan03 <venkatesenj@gmail.com>
2024-05-29 14:08:45 -0700

    Add files via upload

---
+++ day1/FindOddNumbers.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class FindOddNumbers {
+	public static void main(String[] args) {
+		for(int i = 9; i > 0; i--) {
+			if(i % 2 != 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
---
+++ day1/FirstProgram.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		float browserVersion = 10.2f;
+	    String browserName = "chorme";
+		boolean isVisible = true;
+		int relaseYear = 2000;
+		char browserLogo = 'G';
+		System.out.println("browserVersion :"+browserVersion+ "\n"+"browserName :"+browserName+"\n"+"isVisible :"+isVisible+"\n"+"relaseYear :"+relaseYear+"\n"+"browserLogo :"+browserLogo);
+	}
+
+}
